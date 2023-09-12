1df0b22cf901be8d3a94f6673d4e886d19b717e7
Vytautas Valiulis <vytautasval40@gmail.com>
2023-09-12 14:33:14 +0300

    pretalk sesh

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ cur = con.cursor()
 #cur.execute("CREATE TABLE customer_info(email VARCHAR(40) PRIMARY KEY, password VARCHAR(20))")
 #cur.execute("DROP TABLE customer_info")
 
-#cur.execute("INSERT INTO customer_info VALUES('test_1@gmail.com', 'testerissimo')")
+#cur.execute("INSERT INTO customer_info VALUES('test_2@gmail.com', 'testerissimo2')")
 #con.commit()
 
 class BankingApp:
@@ -16,28 +16,38 @@ class BankingApp:
         email = input("Enter your email address: ")
         password = input("Enter your pasword: ")
 
-        '''If email in login database, check if password valid'''
-        return self.login_is_valid(email, password)
+        '''If email in customer_info database, check if password valid'''
+        if self.email_is_valid(email):
+            return self.password_is_valid(email, password)
 
 
-    def login_is_valid(self, input_email, input_password):
+
+    def email_is_valid(self, input_email: str) -> bool:
         '''Checking if email is in the database'''
         cur.execute("SELECT email FROM customer_info")
         email_result = cur.fetchall()
+        print(email_result)
         for registered_email_tuple in email_result:
-            #Tuple unpacking here
-            (registered_email,) = registered_email_tuple
 
+            (registered_email,) = registered_email_tuple #Tuple unpacking here
             if registered_email != input_email:
                 continue
             elif registered_email == input_email:
-                print("well done")
-        return print("Account not found. Please register.")
+                return True
+
+        print("Account not found. Please register.")
+        return False
+
+    def password_is_valid(self, input_email: str, input_password: str):
+        '''Checking if password corresponds to email.'''
+        cur.execute(f"SELECT password FROM customer_info WHERE email = {input_email}")
 
 def main():
     print("Welcome, this is an interactive banking app. Please login to begin.")
     banking_app = BankingApp()
-    banking_app.login()
+    if banking_app.login():
+        ...
+    else:
 
 main()
 
