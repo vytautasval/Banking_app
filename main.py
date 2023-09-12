a6c4ab480ce402976143d662da9023036991d211
Vytautas Valiulis <vytautasval40@gmail.com>
2023-09-12 13:35:17 +0300

    first step

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import re
 
 con = sqlite3.connect("banking_database.db")
 cur = con.cursor()
@@ -9,5 +10,36 @@ cur = con.cursor()
 #cur.execute("INSERT INTO customer_info VALUES('test_1@gmail.com', 'testerissimo')")
 #con.commit()
 
-res = cur.execute("SELECT email FROM customer_info")
-res.fetchall()
+class BankingApp:
+
+    def login(self):
+        email = input("Enter your email address: ")
+        password = input("Enter your pasword: ")
+
+        '''If email in login database, check if password valid'''
+        return self.login_is_valid(email, password)
+
+
+    def login_is_valid(self, input_email, input_password):
+        '''Checking if email is in the database'''
+        cur.execute("SELECT email FROM customer_info")
+        email_result = cur.fetchall()
+        for registered_email_tuple in email_result:
+            #Tuple unpacking here
+            (registered_email,) = registered_email_tuple
+
+            if registered_email != input_email:
+                continue
+            elif registered_email == input_email:
+                print("well done")
+        return print("Account not found. Please register.")
+
+def main():
+    print("Welcome, this is an interactive banking app. Please login to begin.")
+    banking_app = BankingApp()
+    banking_app.login()
+
+main()
+
+
+
