3ed06c69040ab6cda9192562ccb310951883442e
Vytautas Valiulis <vytautasval40@gmail.com>
2023-09-13 21:36:25 +0300

    deposit function done, typehints updated

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ class ProgramInit:
             "Type 'quit' to quit the program.": "quit",
         }
 
-    def start_up(self):
+    def start_up(self) -> str:
+        """Prints out available options for user to take at the first screen and checks if choice valid."""
         print("Welcome, this is an interactive banking app.")
         for prompt, comm in self.command_prompts_1.items():
             print(prompt)
@@ -33,7 +34,8 @@ class ProgramInit:
         else:
             return user_choice
 
-    def account_actions_start_up(self):
+    def account_actions_start_up(self) -> str:
+        """Prints out available options for user to take at the second screen and checks if choice valid."""
         for prompt, comm in self.command_prompts_2.items():
             print(prompt)
 
@@ -47,6 +49,7 @@ class ProgramInit:
             return user_choice
 
     def main(self):
+        """Initializes the first screen to login or register based on user choice."""
         banking_app = BankingApp()
         while True:
             user_choice = self.start_up()
@@ -78,6 +81,7 @@ class ProgramInit:
                 raise (SystemExit("Program stopped."))
 
     def account_actions_main(self, email: str):
+        """Initializes the seconds screen based on user choice."""
         banking_app = BankingApp()
         while True:
             user_choice = self.account_actions_start_up()
@@ -90,7 +94,8 @@ class DatabaseActions:
         self.cursor = self.connection.cursor()
 
     def create_customer_profile(self, email: str, hashed_password: str):
-        """Adds a new email and password into the customer_info database. Password is hashed using bcrypt"""
+        """Adds a new email and password into the customer_info database. Password is hashed using bcrypt.
+        Also adds email to customer_balance database."""
         self.cursor.execute(
             "INSERT INTO customer_info (email, password) VALUES(?, ?)",
             (email, hashed_password),
@@ -101,21 +106,24 @@ class DatabaseActions:
         )
         self.connection.commit()
 
-    def get_email(self, email: str):
+    def get_email(self, email: str) -> str:
+        """Enters customer_info database and returns selected email."""
         self.cursor.execute("SELECT email FROM customer_info WHERE email = ?", (email,))
         email_result_tuple = self.cursor.fetchone()
         (unpacked_email,) = email_result_tuple  # Tuple unpacking
 
         return unpacked_email
 
-    def get_balance(self, email: str):
+    def get_balance(self, email: str) -> int:
+        """Enters customer_balance database and returns balance based on email."""
         self.cursor.execute("SELECT balance FROM customer_balance WHERE email = ?", (email,))
         balance_result_tuple = self.cursor.fetchone()
         (unpacked_balance,) = balance_result_tuple
 
         return unpacked_balance
 
-    def get_deposit(self, email: str):
+    def get_deposit(self, email: str) -> int:
+        """Enters customer_balance database and returns total deposit amount based on email."""
         self.cursor.execute("SELECT deposit FROM customer_balance WHERE email = ?", (email,))
         deposit_result_tuple = self.cursor.fetchone()
         (unpacked_deposit,) = deposit_result_tuple
@@ -123,14 +131,15 @@ class DatabaseActions:
         return unpacked_deposit
 
     def make_deposit(self, email: str, total_amount: float):
-
+        """Updates the customer_balance database with a new total amount for deposits."""
         self.cursor.execute(
             "UPDATE customer_balance SET deposit = ? WHERE email = ?",
             (total_amount, email,)
         )
         self.connection.commit()
 
-    def get_withdrawal(self, email: str):
+    def get_withdrawal(self, email: str) -> int:
+        """Enters customer_balance database and returns total withdrawal amount based on email."""
         self.cursor.execute("SELECT withdrawal FROM customer_balance WHERE email = ?", (email,))
         withdrawal_result_tuple = self.cursor.fetchone()
         (unpacked_withdrawal,) = withdrawal_result_tuple
@@ -179,7 +188,7 @@ class BankingApp(DatabaseActions):
         else:
             return False
 
-    def register(self):
+    def register(self) -> str | None:
         """Prompts the user for their desired email and password and sends them through validity checks.
         If all checks passed, the new user information is stored in the database."""
 
@@ -218,16 +227,17 @@ class BankingApp(DatabaseActions):
 
     def email_format_is_valid(self, input_email: str) -> bool | str:
         """Checks if user inputted email is in a valid format using regex."""
-
         return re.search(
             "([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+",
             input_email,
         )
 
     def password_format_is_valid(self, input_password: str) -> bool | str:
+        """Makes a simple check if password is valid using regex."""
         return re.search(".{8,20}$", input_password)
 
-    def customer_info(self, email: str):
+    def customer_info(self, email: str) -> print:
+        """Prints out the balance, deposit and withdrawal amounts of an account."""
         e = self.get_email(email)
         email_list = e.split("@")
         name = email_list[0]
@@ -240,7 +250,7 @@ class BankingApp(DatabaseActions):
                 f"Your balance is: {balance}\n"
                 f"Split into deposits: {deposit}, and withdrawals: {withdrawal}")
 
-    def number_is_valid(self, input_number: str):
+    def number_is_valid(self, input_number: str) -> bool:
         """Checks if given number is a valid float."""
         try:
             float_number = float(input_number)
